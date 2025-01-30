5b7e72c7627ddb534e9b0525f52d150c6f543c3d
deezinn <developer.andrelz@gmail.com>
2025-01-30 16:51:06 -0300

    alterando o get

--- etl/base_extract.py
+++ etl/base_extract.py
@@ -1,34 +1,36 @@
 from requests import Request, Session
 import json
-from abc import ABC, abstractmethod
 
 def get():
 
     caminho_json = "../ibge.json"
 
     try:
-        with open(caminho_json, "r", encoding="utf-8") as file:
-            dados = json.load(file)
-            nomeLinks = list(dados['links'].keys())
-            links = list(dados['links'].values())
+        try:
+            with open(caminho_json, "r", encoding="utf-8") as file:
+                dados = json.load(file)
+                nomeLinks = list(dados['links'].keys())
+                links = list(dados['links'].values())
 
-    except FileNotFoundError:
-        print(f'Arquivo {caminho_json} não encontrado.')
-        return
+        except FileNotFoundError:
+            print(f'Arquivo {caminho_json} não encontrado.')
+            return
 
-    except KeyError:
-        print(f'Chave dos Links não encontrada no arquivo JSON.')
-        return
+        except KeyError:
+            print(f'Chave dos Links não encontrada no arquivo JSON.')
+            return
 
-    s = Session()
+        s = Session()
 
-    for i in range(len(links)):
-        req = Request("GET", links[i])
-        prepped = s.prepare_request(req)
-        resp = s.send(prepped)
-        print(f"O retorno da api {nomeLinks[i]} foi: {resp.status_code}")
+        for i in range(len(links)):
+            req = Request("GET", links[i])
+            prepped = s.prepare_request(req)
+            resp = s.send(prepped)
+            print(f"O retorno da api {nomeLinks[i]} foi: {resp.status_code}")
 
-    s.close()
+        s.close()
+    except Exception as e:
+        print(f"Erro na requisição", e)
 
 get()
 
