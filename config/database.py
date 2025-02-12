480ef700efb9fad45c46d105484c2a90bbb25d06
Deezinn <developer.andrelz@gmail.com>
2025-02-11 21:11:07 -0300

    :add: adicionando conexão com o banco

---
+++ config/database.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+database_info = {
+    "host" : os.getenv('DB_HOST'),
+    "port" : os.getenv('DB_PORT'),
+    "user" : os.getenv('DB_USER'),
+    'password' : os.getenv('DB_PASSWORD'),
+    "dbname" : os.getenv('DB_NAME')
+    }
+
---
+++ etl/load_data.py
@@ -0,0 +1,26 @@
+import psycopg2
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+from config.database import database_info
+
+
+
+def load():
+
+    conn = psycopg2.connect(host=database_info['host'],
+                            port=database_info['port'],
+                            user=database_info['user'],
+                            password=database_info['password'],
+                            dbname=database_info['dbname'],)
+    print('conectou')
+
+    cur = conn.cursor()
+
+    # aqui ficará o insert dos dados tratados em csv
+
+    cur.close()
+    conn.close()
+    print('desconectou do banco')
+
+if __name__ == "__main__":
+    load()
