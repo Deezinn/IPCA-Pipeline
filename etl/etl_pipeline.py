1189181d03212fab9c29fdb7d3363e154ba2acf1
Deezinn <developer.andrelz@gmail.com>
2025-02-20 20:43:56 -0300

    add herança

--- etl/etl_pipeline.py
+++ etl/etl_pipeline.py
@@ -1,7 +1,9 @@
 from base_extract import get
 from transforms.ipca_transform import transform
+form base_extract import get
+from load_data import load
 
-class ETLPipeline():
+class ETLPipeline(get, transform,load):
     def __init__(self):
         self.get = get()
         self.transform = transform()
